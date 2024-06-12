c6e044a4fd68baa2d4287bd24ca6931d54a87256
eminetarim <97104155+EmineCaki@users.noreply.github.com>
2024-06-12 23:36:57 +0300

    Make code readable and understandable

--- src/components/Classes/ClassesButtonGroup.jsx
+++ src/components/Classes/ClassesButtonGroup.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
+
+const buttons = [
+  { id: "yoga", title: "Yoga" },
+  { id: "group", title: "Group" },
+  { id: "solo", title: "Solo" },
+  { id: "stretching", title: "Stretching" },
+];
 
 const ClassesButtonGroup = ({ setActiveClass }) => {
-  const handleButtonClick = (className) => {
-    setActiveClass(className);
-  };
-
   return (
     <div className="button-group">
-      <button id="yoga" onClick={() => handleButtonClick('yoga')}>Yoga</button>
-      <button id="group" onClick={() => handleButtonClick('group')}>Group</button>
-      <button id="solo" onClick={() => handleButtonClick('solo')}>Solo</button>
-      <button id="stretching" onClick={() => handleButtonClick('stretching')}>Stretching</button>
+      {buttons.map((button) => (
+        <button
+          key={button.id}
+          id={button.id}
+          onClick={() => setActiveClass(button.id)}
+        >
+          {button.title}
+        </button>
+      ))}
     </div>
   );
 };
